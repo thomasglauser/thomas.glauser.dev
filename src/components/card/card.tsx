c2df975a5fdfcd648250e3cc01e249bfdd662622
Thomas Glauser <thomas.glauser01@protonmail.ch>
2023-03-21 17:56:18 +0100

    switch to webp image & optimize styles

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -4,26 +4,26 @@ import Icon from './icon';
 import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
-import profilePic from '/public/img/profilePic.jpeg';
+import profilePic from '/public/img/profilepicture.webp';
 
 export default function Card() {
     return (
-        <div className="xl:w-1/4 sm:w-1/2 w-full 2xl:w-1/5 flex flex-col items-center py-16 md:py-12 bg-white rounded-lg">
+        <div className="m-5 xl:w-1/4 sm:w-1/2 w-full 2xl:w-1/5 flex flex-col items-center py-16 md:py-12 bg-white rounded-lg">
             <div className="w-full flex items-center justify-center">
                 <div className="flex flex-col items-center">
                     <Image
                         className="focus:outline-none rounded-full"
                         src={profilePic}
                         alt="man avatar"
-                        width={150}
-                        height={150}
+                        width={180}
+                        height={180}
                     />
-                    <p className="focus:outline-none mt-4 text-sm md:text-lg font-semibold text-center text-slate-900">
+                    <p className="focus:outline-none mt-8 text-xl font-semibold text-center text-slate-900">
                         Thomas Glauser
                     </p>
                 </div>
             </div>
-            <div className="flex items-center mt-7">
+            <div className="flex items-center mt-8">
                 <div className="">
                     <Icon
                         url="https://github.com/thomasglauser"
